c846f92b1a759f5b14f9b1274f47d458018aeb78
brads <bradstaples28@gmail.com>
2026-04-27 19:03:53 -0700

    4/27

--- frogData.R
+++ frogData.R
@@ -1,39 +1,45 @@
 library(stats)
 library(MASS)
 
-frog<-read.csv("FrogTrackingHab_2026.csv", skip=1)
+frog<-read.csv("FrogTrackingHab_2026.csv", skip=1, check.names=FALSE)
 
 #cleanfrogs1 but a subset where frog id does not cointain r1 or r2 but it does cointain an ID, ie not blank
-cleanFrogs1<-subset(frog, !grepl("R1|R2", Frog.ID) & Frog.ID != "")
+cleanFrogs1 <- subset(frog, !grepl("R1|R2", `Frog ID`) & `Frog ID` != "")
 cleanFrogs1$frogPresence <- 1
-cleanFrogs2<-subset(frog, grepl("R1|R2", Frog.ID))
+
+cleanFrogs2 <- subset(frog, grepl("R1|R2", `Frog ID`))
 cleanFrogs2$frogPresence <- 0
 
 #create a DF of desired columns
-columns<-c("frogPresence", "Fern.Density", "Canopy.cover....","Frond.count",
-           "Fern.Health","Fern.diam..cm.", "Fern.Complx", "Plant.sp.", "Slope....deg.",
-           "BG", "LL", "SF", "FB", "TS", "VG", "WD", "WA",
-           "NE", "SW", "NW", "SE","Latitude", "Longitude")
+columns <- c("frogPresence", "Fern Density", "Canopy cover (%)", "Frond count", 
+             "Fern Health", "Fern diam (cm)", "Fern Complx", "Plant sp.", 
+             "Slope   (deg)", "BG", "LL", "SF", "FB", "TS", "VG", "WD", "WA", 
+             "NE", "SW", "NW", "SE", "Latitude", "Longitude")
 
-frogData<-rbind(cleanFrogs1[, columns], cleanFrogs2[, columns])
+frogData <- rbind(cleanFrogs1[, columns], cleanFrogs2[, columns])
 
 #force factors into numerics
-frogData$fernDensity<-as.numeric(frogData$Fern.Density)
-frogData$canopyCover<-as.numeric(frogData$Canopy.cover....)
-frogData$Frond.count<-as.numeric(frogData$Frond.count)
-frogData$Fern.diam..cm.<-as.numeric(frogData$Fern.diam..cm.)
-frogData$Plant.sp.<-as.factor(frogData$Plant.sp.)
-frogData$Fern.Health<-as.factor(frogData$Fern.Health)
+frogData$fernDensity<- as.numeric(frogData$`Fern Density`)
+frogData$canopyCover<- as.numeric(frogData$`Canopy cover (%)`)
+frogData$frondCount<- as.numeric(frogData$`Frond count`)
+frogData$fernDiam<- as.numeric(frogData$`Fern diam (cm)`)
+frogData$plantSp<- as.factor(frogData$`Plant sp.`)
+frogData$fernHealth<- as.factor(frogData$`Fern Health`)
+frogData$slope<- as.numeric(frogData$`Slope   (deg)`)
 
 #removing nas from fern denisty and canopy cover
-frogData<-frogData[!is.na(frogData$fernDensity) & !is.na(frogData$canopyCover) 
-                   & !is.na(frogData$Frond.count)& !is.na(frogData$Fern.diam..cm), ]
+frogData <- frogData[!is.na(frogData$fernDensity) & 
+                       !is.na(frogData$canopyCover) & 
+                       !is.na(frogData$frondCount) & 
+                       !is.na(frogData$fernDiam), ]
 #cleaning dataset
 
 plot(frogData)
 
-frogModel<-glm.nb(frogPresence~fernDensity+canopyCover+Frond.count+Fern.Health+Fern.diam..cm.+
-                 Fern.Complx+Slope....deg.+BG+LL+SF+FB+TS,
-               data=frogData)
+frogModel <- glm.nb(frogPresence ~ fernDensity + canopyCover + frondCount + 
+                      fernHealth + fernDiam + `Fern Complx` + slope + 
+                      BG + LL + SF + FB + TS, 
+                    data = frogData)
+
 summary(frogModel)
 
--- soilData.R
+++ soilData.R
@@ -1,8 +1,21 @@
 library(tidyr)
-soil<-read.csv("WetSeasonRandomSoil_2025.csv")
-season<-read.csv("WetSeasonRandomHab_2025.csv",skip=1)
+library(dplyr)
+soil<-read.csv("WetSeasonRandomSoil_2025.csv", check.names = FALSE)
+season<-read.csv("WetSeasonRandomHab_2025.csv",skip=1, check.names = FALSE)
+###################
+seasonClean<-fill(season, 'Point #', .direction = "down")
+seasonClean<-unite(seasonClean, col='Point #', c('Point #', 'Soil sample'), sep="", remove=FALSE)
+data <- left_join(soil, seasonClean, by = c("Soil Smpl #" = "Point #"))
 
-soilClean<-separate(soil, col='Soil.Smpl..',
-                    into=c('Point #', 'Soil Sample'), sep=-1)
-head(soilClean)
-seasonClean<-season[rep(1:nrow(season), by="Point.." each = 4), ]
+head(data)
+#save mollusks as numeric
+data$`# mollusks` <- as.numeric(data$`# mollusks`)
+
+#save soil sample as a factor
+data$`Soil sample` <- as.factor(data$`Soil sample`)
+#does the presence of ferns affect microhabitat characteristics such as soil char, number of molllusk
+#and number of burrows, as well as soil parameters.
+mod1<-lm(`% SM` ~ `Soil sample`, data=data)
+summary(mod1)
+mod2<-lm(`% OM` ~ `Soil sample`, data=data)
+summary(mod2)
